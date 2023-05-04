c1f78b04cac7870964b4196269e2489aa511fb44
Marco Bonifacio <bonifacio.marco@gmail.com>
2023-05-04 17:21:10 +0200

    class pdfmerge

--- src/merge.py
+++ src/merge.py
@@ -20,24 +20,30 @@ def modify_canvas():
     js.document.getElementById('action-button').style.visibility = 'visible'
 
 
-def read_pdf(evt):
-    return PdfReader(io.BytesIO(bytes(evt.target.result.to_py())))
+class PdfMerge:
 
+    def __init__(self):
+        self.files = []
+    
+    def read_pdf(self, evt):
+        pdf = PdfReader(io.BytesIO(bytes(evt.target.result.to_py())))
+        js.console.log(len(pdf.pages))
 
-def select_files(evt):
-    filelist = evt.target.files
-    modify_canvas()
-        
-    for f in filelist:
-        Element('target').write(f.name, append=True)
-        #reader = js.FileReader.new()
-        #reader.onload = create_proxy(read_pdf)
-        #reader.readAsArrayBuffer(f)
+    def select_files(self, evt):
+        filelist = evt.target.files
+        modify_canvas()
+        for f in filelist:
+            self.files.append(f)
+            Element('target').write(f.name, append=True)
+            reader = js.FileReader.new()
+            reader.onload = create_proxy(self.read_pdf)
+            reader.readAsArrayBuffer(f)
     
 
 def setup():
     add_event_listener(js.document.getElementById('selector'),
-                       'change', select_files)
+                       'change', pdf_merge.select_files)
 
 
+pdf_merge = PdfMerge()
 setup()
