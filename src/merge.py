0450b2d8a8400fb496a86caa50080018d7f308a9
Marco Bonifacio <bonifacio.marco@gmail.com>
2023-05-05 16:41:11 +0200

    v0.1

--- src/merge.py
+++ src/merge.py
@@ -1,8 +1,10 @@
+import asyncio
 import io
 import js
 from pyscript import Element
+from pyodide.ffi import to_js
 from pyodide.ffi.wrappers import add_event_listener, create_proxy
-from pypdf import PdfReader
+from pypdf import PdfWriter
 
 
 def show_menu():
@@ -18,26 +20,51 @@ def modify_canvas():
     js.document.getElementById('description').style.display = 'none'
     js.document.getElementById('select-container').style.display = 'none'
     js.document.getElementById('action-button').style.visibility = 'visible'
+    js.document.getElementById('add-container').style.visibility = 'visible'
+    add_event_listener(js.document.getElementById('add-selector'),
+                       'change', pdf_merge.select_files)
+    add_event_listener(js.document.getElementById('action-button'),
+                       'click', pdf_merge.merge_files)
 
 
 class PdfMerge:
 
     def __init__(self):
         self.files = []
+        self.merger = PdfWriter()
     
-    def read_pdf(self, evt):
-        pdf = PdfReader(io.BytesIO(bytes(evt.target.result.to_py())))
-        js.console.log(len(pdf.pages))
-
+    def write_pdf(self, evt):
+        self.merger.append(io.BytesIO(bytes(evt.target.result.to_py())))
+        
     def select_files(self, evt):
+        if evt.target.id == 'selector':
+            modify_canvas()
         filelist = evt.target.files
-        modify_canvas()
         for f in filelist:
             self.files.append(f)
             Element('target').write(f.name, append=True)
+    
+    async def merge_files(self, evt):
+        for f in self.files:
             reader = js.FileReader.new()
-            reader.onload = create_proxy(self.read_pdf)
+            reader.onload = create_proxy(self.write_pdf)
             reader.readAsArrayBuffer(f)
+        await asyncio.sleep(2)
+        output = io.BytesIO()
+        self.merger.write(output)
+        self.merger.close()
+        output.seek(0)
+        content = to_js(output.read())
+        blob = js.Blob.new([content], {type: "application/pdf"})
+        blob_url = js.window.URL.createObjectURL(blob)
+        templink = js.document.createElement('a')
+        templink.style.display = 'none'
+        templink.href = blob_url
+        templink.setAttribute('download', 'merged_pdf.pdf')
+        js.document.body.appendChild(templink)
+        templink.click()
+        js.document.body.removeChild(templink)
+        js.window.URL.revokeObjectURL(blob_url)
     
 
 def setup():
